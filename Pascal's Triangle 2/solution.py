0111961c4b7dbc0ebb5811846b6fb53d78bd3724
David Mitton <75094969+dmitton@users.noreply.github.com>
2022-11-09 13:28:13 -0700

    Create solution.py
    Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.

---
+++ Pascal's Triangle 2/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        answer = []
+        for i in range(rowIndex + 1):
+            newRow = []
+            if i == 0:
+                newRow.append(1)
+                answer.append(newRow)
+            else:
+                counter = 1
+                while counter <= i + 1:
+                    if counter == 1 or counter == (i + 1):
+                        newRow.append(1)
+                        counter = counter + 1
+                    else:
+                        num1 = counter - 1
+                        num2 = counter - 2
+                        number1 = answer[i - 1][num1]
+                        number2 = answer[i - 1][num2]
+                        newNumber = number1 + number2
+                        newRow.append(newNumber)
+                        counter = counter + 1
+                answer.append(newRow)
+        return answer[rowIndex]
